2d11a734a31080c640ec6c3f22534fc8c3e474b4
Brian Takita <brian.takita@gmail.com>
2020-01-04 17:20:51 -0500

    feat: store: b__ factories
    ∋ "@ctx-core/object": "^6.5.0"

--- store.d.ts
+++ store.d.ts
@@ -1,4 +1,9 @@
-export declare const __opened__nav: import("svelte/store").Writable<boolean>;
-export declare function toggle__nav(): void;
-export declare function open__nav(): void;
-export declare function close__nav(): void;
+import { Writable } from 'svelte/store';
+export interface Writable__opened__nav extends Writable<boolean> {
+    toggle__nav: () => void;
+    open__nav: () => void;
+    close__nav: () => void;
+}
+export declare const b__opened__nav: (ctx?: any, opts?: any) => Writable__opened__nav;
+export declare const __opened__nav: Writable__opened__nav;
+export declare const open__nav: () => void, close__nav: () => void, toggle__nav: () => void;
--- store.ts
+++ store.ts
@@ -1,12 +1,31 @@
-import { writable } from 'svelte/store'
+import { Writable, writable } from 'svelte/store'
+import { _b, assign } from '@ctx-core/object'
 import { not } from '@ctx-core/function'
-export const __opened__nav = writable(false)
-export function toggle__nav() {
-  __opened__nav.update(not)
+export interface Writable__opened__nav extends Writable<boolean> {
+	toggle__nav:()=>void
+	open__nav:()=>void
+	close__nav:()=>void
 }
-export function open__nav() {
-  __opened__nav.set(true)
-}
-export function close__nav() {
-  __opened__nav.set(false)
-}
+export const b__opened__nav = _b<Writable__opened__nav>('__opened__nav', ()=>{
+	const __opened__nav = writable(false)
+	return assign(__opened__nav, {
+		open__nav,
+		close__nav,
+		toggle__nav,
+	})
+	function toggle__nav() {
+		__opened__nav.update(not)
+	}
+	function open__nav() {
+		__opened__nav.set(true)
+	}
+	function close__nav() {
+		__opened__nav.set(false)
+	}
+})
+export const __opened__nav = b__opened__nav()
+export const {
+	open__nav,
+	close__nav,
+	toggle__nav,
+} = __opened__nav
