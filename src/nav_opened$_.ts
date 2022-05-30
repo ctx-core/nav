24cc6b155d00fb10127bc6dbb870259abf087b4a
Brian Takita <brian.takita@gmail.com>
2022-05-30 01:40:27 -0500

    update dependencies

--- src/nav_opened$_.ts
+++ src/nav_opened$_.ts
@@ -1,7 +1,7 @@
 import { assign, B, be_ } from '@ctx-core/object'
-import { atom$, WritableAtom$ } from '@ctx-core/nanostores'
+import { atom_, WritableAtom_ } from '@ctx-core/nanostores'
 export const nav_opened$_:B<nav_opened$_T> = be_('nav_opened$', ()=>{
-	const nav_opened$ = atom$(false)
+	const nav_opened$ = atom_(false)
 	return assign(nav_opened$, {
 		open_nav,
 		close_nav,
@@ -17,7 +17,7 @@ export const nav_opened$_:B<nav_opened$_T> = be_('nav_opened$', ()=>{
 		nav_opened$.$ = false
 	}
 })
-export interface nav_opened$_T extends WritableAtom$<boolean> {
+export interface nav_opened$_T extends WritableAtom_<boolean> {
 	toggle_nav:()=>void
 	open_nav:()=>void
 	close_nav:()=>void
